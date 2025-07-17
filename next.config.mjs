89522a5d796d08e039cc9f541df55adea19667e3
Svitlana Horodylova <horodylova.sv@gmail.com>
2025-07-17 14:23:16 +0100

    added configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/portfolio-new.png',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
